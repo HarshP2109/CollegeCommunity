5feeb20697ddceabc4c0e51f7edb2b0ae3e0f128
Harsh <harshpimpale@gmail.com>
2024-04-10 08:22:25 +0530

    Bug Fix #3

--- src/utils/dashboardFunctions.js
+++ src/utils/dashboardFunctions.js
@@ -75,28 +75,34 @@ async function updateActivityTable(email) {
 }
 
 
-async function extractActivityData(id){
+async function extractActivityData(Id){
 
-    let date = await dailyData.findOne({ UniqueId: Id });
+    let data = await dailyData.findOne({ UniqueId: Id });
 
-    let sendingData = {
-        'date1': formatDate(data["Date1"]),
-        'Active1': data['DateActive1'],
-        'date2': formatDate(data["Date2"]),
-        'Active2': data['DateActive2'],
-        'date3': formatDate(data["Date3"]),
-        'Active3': data['DateActive3'],
-        'date4': formatDate(data["Date4"]),
-        'Active4': data['DateActive4'],
-        'date5': formatDate(data["Date5"]),
-        'Active5': data['DateActive5'],
-        'date6': formatDate(data["Date6"]),
-        'Active6': data['DateActive6'],
-        'date7': "Today",
-        'Active7': data['DateActive7'],
+    if (!data) {
+        console.log(Id + " is not in database");
+        throw new Error('Data not found');
+    }else{
+
+        let sendingData = {
+            'date1': formatDate(data["Date1"]),
+            'Active1': data['DateActive1'],
+            'date2': formatDate(data["Date2"]),
+            'Active2': data['DateActive2'],
+            'date3': formatDate(data["Date3"]),
+            'Active3': data['DateActive3'],
+            'date4': formatDate(data["Date4"]),
+            'Active4': data['DateActive4'],
+            'date5': formatDate(data["Date5"]),
+            'Active5': data['DateActive5'],
+            'date6': formatDate(data["Date6"]),
+            'Active6': data['DateActive6'],
+            'date7': "Today",
+            'Active7': data['DateActive7'],
+        }
+        return sendingData;
+    
     }
-
-    return sendingData;
 
 }
 
